3b7acb7c2dc7b089c2cfa9dececced04f0c81e6c
Brandon Ruiz <Sober4202020@outlook.com>
2024-01-28 07:04:15 +0000

    adding red

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import React from 'react';
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-
-
+import Scrollin from './Components/Scrollin';
 
 
 function App() {
-  return (
-	  <Card style={{width: '20rem'}}>
-	  	<Card.Img variant="top" src=""/>
-	  	<Card.Body>
-	  		<Card.Title>
-	  Gym Barbell
-	  		</Card.Title>
-	  <Card.Text>
-	  Testing and then more testing and after that, probably might do some testing
-	  </Card.Text>
-	  <Button variant="primary">Testing</Button>
-	  	</Card.Body>
-	  </Card>
-  );
+	  return (
+		      <Scrollin/>
+		    );
 }
 
 export default App;
