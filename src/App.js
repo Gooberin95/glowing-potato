0887089c5149fa3ad648bc2e28042c624df1e2ae
Brandon Ruiz <Sober4202020@outlook.com>
2024-01-28 19:36:11 +0000

    adding layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-
 import React from 'react';
-import Hello from './Hello';
+import './App.css';
+
 const App = () => {
 	  return (
-		      <div>
-		        <Hello />
+		      <div className="app">
+		        <Navbar />
+		        <div className="split-layout">
+		          <div className="left">
+		            <img src="your_photo.jpg" alt="Your Photo" />
+		          </div>
+		          <div className="right">
+		            <h1>Welcome to My Website</h1>
+		            <p>This is a simple example of a split-layout React website.</p>
+		            <p>Add your content here.</p>
+		          </div>
+		        </div>
 		      </div>
 		    );
 };
-export default App; 
+
+const Navbar = () => {
+	  return (
+		      <nav className="navbar">
+		        <ul>
+		          <li>Home</li>
+		          <li>About</li>
+		          <li>Contact</li>
+		        </ul>
+		      </nav>
+		    );
+};
+
+export default App;
