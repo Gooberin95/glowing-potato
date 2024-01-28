6866574f7ebde86cef8b4f951dc195f4f95cfc2d
Brandon Ruiz <Sober4202020@outlook.com>
2024-01-28 08:08:16 +0000

    adding screen saver

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import React from 'react';
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-
-
-
-
-function App() {
-  return (
-	  <Card style={{width: '20rem'}}>
-	  	<Card.Img variant="top" src=""/>
-	  	<Card.Body>
-	  		<Card.Title>
-	  Gym Barbell
-	  		</Card.Title>
-	  <Card.Text>
-	  Testing and then more testing and after that, probably might do some testing
-	  </Card.Text>
-	  <Button variant="primary">Testing</Button>
-	  	</Card.Body>
-	  </Card>
-  );
-}
+import Hello from './Hello';
+const App = () => {
+	  return (
+		      <div>
+		        <Hello />
+		      </div>
+		    );
+};
 
 export default App;
+
---
+++ src/aboutdata.js
@@ -0,0 +1,5 @@
+const aboutdata = `Welcome to Visionary GC LLC, your trusted partner in turning dreams into reality. As a leading general contractor specializing in home renovations and construction, we take pride in transforming spaces and creating homes that reflect your unique vision. With a commitment to excellence, we bring a wealth of experience, craftsmanship, and creativity to every project. Our team at Visionary GC is dedicated to providing personalized solutions, ensuring that each renovation or construction endeavor is a seamless and rewarding experience for our clients. Whether it's revamping your living space or building your dream home, we are here to bring your ideas to life.`
+
+
+
+export default aboutdata
