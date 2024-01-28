9cbb1ead81a8526b3c58bf7eb42295812e6fe3f3
Brandon Ruiz <Sober4202020@outlook.com>
2024-01-28 19:12:00 +0000

    adding

--- src/Hello.js
+++ src/Hello.js
@@ -5,14 +5,7 @@ import sheep from './sheep.jpg';
 
 function Hello() {
 	  return (
-		      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-		        <Card style={{ width: '40rem' }}>
-		          <Card.Img variant="top" src={sheep} />
-		          <Card.Body>
-		            <Card.Title>Hello Mavis! Thank you for waiting and being patient!</Card.Title>
-		          </Card.Body>
-		        </Card>
-		      </div>
+		  <h1>Good morning</h1>
 		    );
 }
 
